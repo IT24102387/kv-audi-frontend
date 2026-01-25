c538f8021eba1a3b22812bb9ad6f9b70aae1b6da
Kusal Kolambage <IT24102387@my.sliit.lk>
2026-01-25 17:47:45 +0530

    admin pages update

---
+++ src/Pages/admin/adminItems.jsx
@@ -0,0 +1,13 @@
+import { CiCirclePlus } from "react-icons/ci";
+import { Link } from "react-router-dom";
+
+export default function AdminItemPage(){
+    return(
+        <div className="w-full h-full relative">
+            <Link to="/admin/items/add">
+               <CiCirclePlus className="text-[70px] absolute right-3 bottom-2 hover:text-red-700 "/>
+            </Link>
+        </div>
+    )
+
+}
--- src/Pages/admin/adminPage.jsx
+++ src/Pages/admin/adminPage.jsx
@@ -2,13 +2,15 @@ import { BsGraphDown } from "react-icons/bs";
 import { FaRegBookmark, FaRegUser } from "react-icons/fa";
 import { MdOutlineSpeaker } from "react-icons/md";
 import { Link, Route, Routes } from "react-router-dom";
+import AdminItemPage from "./adminItems";
+import AddItemPage from "./addItemPage";
 
 
 
 export default function AdminPage(){
     return(
     <div className='w-full h-screen flex ' >
-        <div className='w-[300px] h-full bg-green-200'>
+        <div className='w-[200px] h-full bg-green-200'>
 
         <button className='w-full h-[40px] text-[25px] font-bold bg-red-400 flex justify-center items-center' >
           <BsGraphDown/>
@@ -28,11 +30,12 @@ export default function AdminPage(){
 
 
       </div>
-      <div className='w-[calc(100vw-300px)]  bg-blue-900'>
+      <div className='w-[calc(100vw-200px)] '>
         <Routes path="/*">
-         <Route path="/bookings" element={<h1>booking</h1>}/>
-         <Route path="/items" element={<h1>itemns</h1>}/>
+         <Route path="/bookings" element={<h1>bookings</h1>}/>
          <Route path="bookings/*" element ={<h1>Not found</h1>}/>
+         <Route path="/items" element={<AdminItemPage/>}/>
+         <Route path="/items/add" element={<AddItemPage/>}/>
         </Routes>
      
 
